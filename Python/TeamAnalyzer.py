1f30e8d22570b3db2d3addaeaa3f3f01b7a48651
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-05-02 14:18:50 -0700

    Update TeamAnalyzer.py

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -10,13 +10,14 @@ types = ["bug","dark","dragon","electric","fairy","fight",
 if len(sys.argv) < 6:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
-team = sys.argv[1:]
+team=[sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6]]
 
 conn = sqlite3.connect('pokemon.sqlite')
 cursor = conn.cursor()
 
-
-for i in team:
+for i, arg in enumerate(sys.argv):
+    if i == 0:
+        continue
     result = cursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = {}".format(i))
     pokemon_list = list(cursor.fetchone())
     strong = []
