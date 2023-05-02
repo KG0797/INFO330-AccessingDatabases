f7cc30694add1dc440d6739453c8c17f10fa340f
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-05-02 14:22:04 -0700

    Update TeamAnalyzer.py

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -18,6 +18,7 @@ cursor = conn.cursor()
 for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
+ for i in team:
     result = cursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = {}".format(i))
     pokemon_list = list(cursor.fetchone())
     strong = []
