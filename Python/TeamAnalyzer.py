4a9d853e305d6cb0b415e8ef14295d0fe0daccee
KG0797 <121983886+KG0797@users.noreply.github.com>
2023-05-02 13:27:18 -0700

    Update TeamAnalyzer.py

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -20,21 +20,21 @@ for i, arg in enumerate(sys.argv):
         continue
 <<<<<<< HEAD    
         
-for i in team:
-    result = cursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = {}".format(i))
-    pokemon_list = list(cursor.fetchone())
-    strong = []
-    weak = []
-    against_list = pokemon_list[3:]
-    
-    for a in range(len(against_list)):
-        if float(against_list[a]) > 1:
-            strong.append(types[a])
-        elif float(against_list[a]) < 1:
-            weak.append(types[a])
-        else:
-            continue
-    print("{} ({} {}) is strong against {} but weak against {}".format(pokemon_list[0], pokemon_list[1], pokemon_list[2], strong, weak))  
+    for i in team:
+        result = cursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = {}".format(i))
+        pokemon_list = list(cursor.fetchone())
+        strong = []
+        weak = []
+        against_list = pokemon_list[3:]
+
+        for a in range(len(against_list)):
+            if float(against_list[a]) > 1:
+                strong.append(types[a])
+            elif float(against_list[a]) < 1:
+                weak.append(types[a])
+            else:
+                continue
+        print("{} ({} {}) is strong against {} but weak against {}".format(pokemon_list[0], pokemon_list[1], pokemon_list[2], strong, weak))  
  
 >>>>>>> 2375fc41f28b239621ba27253d77697f2854489e
 
